8bb89cc184a0970d8dbd8d9ed3cfbf48940e6b50
David Blevins <31944327+dblevins-3dgfx@users.noreply.github.com>
2023-03-24 20:09:06 +0000

    Remove misleading comments

--- src/modules/BlevinsModule.h
+++ src/modules/BlevinsModule.h
@@ -2,13 +2,12 @@
 #include "SinglePortModule.h"
 
 /**
- * A simple example module that just replies with "Message received" to any message it receives.
+ *
  */
 class BlevinsModule : public SinglePortModule
 {
   public:
     /** Constructor
-     * name is for debugging output
      */
     BlevinsModule() : SinglePortModule("blevins", meshtastic_PortNum_PRIVATE_APP) {}
 };
